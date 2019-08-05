094a2c4e0df1b6c67751ba36605305098e23fc51
jjwangel <93108@qq.com>
2019-08-05 16:45:53 +0800

    20190805

--- src/view/single-page/home/home_level1.vue
+++ src/view/single-page/home/home_level1.vue
@@ -71,7 +71,8 @@
         <ToDo style="margin-bottom: 10px" todo_title="待办事项"></ToDo>
         <Card shadow>
           <div style="height: 544px;text-align: center">
-            <img v-if="showZsMap" :src="zsMap" style="height: 544px;width: 100%;" alt="">
+            <!-- <img v-if="showZsMap" :src="zsMap" style="height: 544px;width: 100%;" alt=""> -->
+            <iframe :src="mapSrc" id="zs_map" scrolling="no" frameborder="0" style="top:0px;left: 0px;width: 100%;height: 100%;"></iframe>
           </div>
         </Card>
       </Col>
@@ -115,7 +116,8 @@ export default {
       showZsMap: this.rolesCodeShort().findIndex((val) => val === 'bms_admin' || val === 'bms_01' || val === 'bms_02') !== -1,
       data_fz: [],
       data_sx: [],
-      zsMap
+      zsMap,
+      mapSrc: 'http://125.0.165.182:3838/proc-apps/focusperson_01/'
     }
   },
   methods: {
