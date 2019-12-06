1603ad7f4856adbf0bd37f3104ddc74b60a69afb
jjwangel <93108@qq.com>
2019-12-06 14:19:08 +0800

    fixbug-20191206

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -1,14 +1,14 @@
 <template>
   <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
     <FormItem prop="userName">
-      <Input v-model="form.userName" placeholder="请输入用户名">
+      <Input v-model="form.userName" :disabled="this.disableLogin" placeholder="请输入用户名">
         <span slot="prepend">
           <Icon :size="16" type="ios-person"></Icon>
         </span>
       </Input>
     </FormItem>
     <FormItem prop="password">
-      <Input type="password" v-model="form.password" placeholder="请输入密码">
+      <Input type="password" v-model="form.password" :disabled="this.disableLogin" placeholder="请输入密码">
         <span slot="prepend">
           <Icon :size="14" type="md-lock"></Icon>
         </span>
@@ -62,7 +62,7 @@ export default {
   },
   methods: {
     handleSubmit () {
-      if (this.logining) return
+      if (this.logining || this.disableLogin) return
 
       this.logining = true
       this.$refs.loginForm.validate((valid) => {
